5c71c8cd61efa44c78d1088ff01cfa7001fe1413
ArunKumar000777 <arunashok1020@gmail.com>
2023-01-15 01:10:48 +0530

    change the base url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://arun-ecommerce-api.com/api/";
 const TOKEN =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYWRiNzEyZDA5OGI2OGUyMzc2MWZiMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjUyNzU2NSwiZXhwIjoxNjU2Nzg2NzY1fQ.IMI3TXd_HHLxNgKmhYahQ-yeySNk61XqmIE1VQGD7Gg";
 
